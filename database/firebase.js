e10a1bfbb913f61ee76be47bb88fe729d66ae2a4
Nicolas Acevedo <acevedonm@gmail.com>
2020-12-16 02:13:40 -0300

    newFeature (/database/firebase.js) se creo la base de datos con firebase  y se conecto a la nube

---
+++ database/firebase.js
@@ -0,0 +1,25 @@
+//ESTE MODULO CONECTA LA BASE DE DATOS EN LA NUBE
+//para utilizarla en front: import firebase from "../database/firebase"
+
+import firebase from "firebase";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD0UiBdd2jsAMehbxyaWFEys9O-RA_MKXM",
+  authDomain: "stackhenryflow.firebaseapp.com",
+  projectId: "stackhenryflow",
+  storageBucket: "stackhenryflow.appspot.com",
+  messagingSenderId: "648957429624",
+  appId: "1:648957429624:web:7d14aaec62a84c0c50ea8d",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export default {
+  firebase,
+  db
+};
+ 
