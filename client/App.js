97751d79261aadb031a2f1bb3d10761a125497ee
Maxi De Filippis <maxidefilippis@gmail.com>
2020-12-16 12:18:53 -0300

    New feature (/client) Agrego Login+Register y carpeta Styles con variables

--- client/App.js
+++ client/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {NavigationContainer} from "@react-navigation/native"
 import {createStackNavigator} from "@react-navigation/stack"
-import Home from "./screens/home"
+import Home from "./screens/Home"
+import Login from "./screens/Login"
+import Register from "./screens/Register"
 import firebase from "../database/firebase"
 
 const Stack = createStackNavigator()
@@ -9,11 +11,11 @@ const Stack = createStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={ { headerShown: false } }>
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Register" component={Register}/>
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword}/>
+        {/* <Stack.Screen name="ForgotPassword" component={ForgotPassword}/> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
