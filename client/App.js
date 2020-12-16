af0bb6b9952a8738c5b08fcf38766c9ac8ab6830
Maxi De Filippis <maxidefilippis@gmail.com>
2020-12-15 21:01:41 -0300

    Fix (/client) Corrección de instalación del Front con EXPO

--- client/App.js
+++ client/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>El front está re copado!</Text>
       <StatusBar style="auto" />
     </View>
   );
