a96f63d6462d7f8ae558b0d5f94c1ffea29d2d6e
OrtizLara <ortizlaraeliana@gmail.com>
2020-12-17 18:39:05 -0300

    /client/components/comments finish

--- client/App.js
+++ client/App.js
@@ -6,18 +6,19 @@ import Register from "./screens/Register"
 import ForgotPassword from "./screens/ForgotPassword"
 // import firebase from "../database/firebase"
 import Welcome from "./screens/Welcome"
-
+import Comments from "./components/Comments"
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator  initialRouteName="Welcome" screenOptions={ { headerShown: false } }>
+      <Stack.Navigator  screenOptions={ { headerShown: false } }>
       
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Register" component={Register}/>
         <Stack.Screen name="ForgotPassword" component={ForgotPassword}/>
         <Stack.Screen name="Welcome" component={Welcome}/>
+        <Stack.Screen name= "Comments" component={Comments}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
