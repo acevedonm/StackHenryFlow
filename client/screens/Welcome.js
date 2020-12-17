89e00fd0343d3f876a40a1c8d98113711ac7d17e
OrtizLara <ortizlaraeliana@gmail.com>
2020-12-17 14:00:46 -0300

    /client/screen/SearchBar finish

--- client/screens/Welcome.js
+++ client/screens/Welcome.js
@@ -1,10 +1,14 @@
 import React from "react"
 import { View, Text, TouchableOpacity } from "react-native"
+import SearchBar from "./SearchBar"
 
 //Aca renderizamos SearchBar, Componente nuevo post y Componente post
 const Welcome = ( { navigation } )=>{
     return (
         <View>
+            <SearchBar/>
+
+            
             <Text>¡Bienvenido a Stack Henry Flow!</Text>
             {/* <TouchableOpacity onPress={() => { navigation.navigate('Login') }}>
                 <Text>Ir al Login</Text>
