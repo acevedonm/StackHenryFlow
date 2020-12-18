09237fd83b393aa742098824aba96bb87bbb0eba
RocioPerez <rocioperez08@outlook.com>
2020-12-18 13:53:15 -0300

    NewFeature (succes/Welcome and succes/NewPostForm) Update Styles

--- client/screens/Welcome.js
+++ client/screens/Welcome.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { View, Text, TouchableOpacity } from "react-native"
+import { View, Text, TouchableOpacity, Image } from "react-native"
 import SearchBar from "./SearchBar"
 import { styles } from "../styles/styles";
 import NewPostForm from "../components/NewPostForm"
@@ -9,17 +9,27 @@ import Post from "../components/Posts"
 //Aca renderizamos SearchBar, Componente nuevo post y Componente post
 const Welcome = ( props, { navigation } )=>{
     return (
+        <>
         <View style={styles.header}>
-            <SearchBar/>
             
-        <View/>
-            <Text>¡Bienvenido a Stack Henry Flow!</Text>
+            <Image
+                source={require("../assets/henry.png")}
+                resizeMode="contain"
+                style={styles.imgHenry}
+             >   
+             </Image>
+             <SearchBar/>  
+            
+        </View>
+        <View style={styles.body}>
+            <Text style={styles.h1}>¡Bienvenido a Stack Henry Flow!</Text>
             <NewPostForm/>
             <Post/>
             {/* <TouchableOpacity onPress={() => { navigation.navigate('NewPostForm') }}>
                 <Text>Postear</Text>
             </TouchableOpacity> */}
         </View>
+        </>
     )
 }
 
