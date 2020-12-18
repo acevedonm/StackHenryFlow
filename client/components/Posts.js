a215a77809ec7de5fb094a8cebd9f2a1879fb1a4
AyeVillarruel <ayelen.villarruel.1998@gmail.com>
2020-12-17 23:22:05 -0300

    fix (/screen/Welcome)(/components/Posts), arregle el componente posts

--- client/components/Posts.js
+++ client/components/Posts.js
@@ -1,37 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 import { ListItem } from 'react-native-elements';
 import {StyleSheet, Text, View, Button, TextInput, TouchableOpacity, Image, FlatList} from 'react-native';
-import Welcome from '../screens/Welcome'
+import ControllerPost from '../../database/controllers/controllerPost'
 
 
 
 export default function Posts ({natigation}){
 
-    const[posts,setPosts ] = useState([])
-
     const getPosteos = async() => {
        //  var posteos= ControllerPost.GetAllPosts()
       
-         const asyncCall = () => {
-           console.log("calling");
-            const response = ControllerPost.GetAllPosts();
-           return response;
-           // expected output: "resolved"
-         };
-         const result = await asyncCall();
-         result && setPosts({ posts : result})
+         const  asyncCall = () =>{
+            console.log('calling');
+            const response =  ControllerPost.GetAllPosts()
+            return response
+            // expected output: "resolved"
+          }
+      const result=  await asyncCall()
+          console.log(result)
       
     }
     
 
 
-   
-    
+    useEffect(()=> {
+     getPosteos()
+
+    },[])
+
     return(
-        
         <ScrollView>
-           
             
         </ScrollView>
     )
