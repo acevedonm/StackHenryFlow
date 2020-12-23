5613a8fb8bad9ed95b2008465cc16bec7ca0b309
OrtizLara <ortizlaraeliana@gmail.com>
2020-12-22 23:26:35 -0300

    components/screens/index-header

--- client/screens/Login/Index.js
+++ client/screens/Login/Index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 // NAVIGATION //
 import { createDrawerNavigator } from "@react-navigation/drawer";
 // COMPONENTS //
@@ -9,8 +9,19 @@ import SideBar from "../../components/SideBar";
 import ProfileEdit from "../User/ProfileEdit"
 import PostsList from "../../components/PostsList"
 import PostDetails from "../../components/PostDetails"
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const USER_LOGIN= '@user_login'
 
 const Drawer = createDrawerNavigator();
+
+function LogOut({navigation}) {
+  useEffect(() => {
+    AsyncStorage.removeItem(USER_LOGIN)
+    navigation.navigate('Login')
+
+  },[])
+  return null;
+}
 
 export default function Index({ navigation }) {
   return (
@@ -19,9 +30,10 @@ export default function Index({ navigation }) {
     >
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="Profile" component={Profile} />
-      <Drawer.Screen name="Comments" component={Comments} />
+      {/* <Drawer.Screen name="Comments" component={Comments} />
       <Drawer.Screen name="PostsList" component={PostsList} />
-      <Drawer.Screen name="PostDetails" component={PostDetails} />
+      <Drawer.Screen name="PostDetails" component={PostDetails} /> */}
+      <Drawer.Screen name="Sign out" component={LogOut} />
       {/* <Drawer.Screen name="ProfileEdit" component={ProfileEdit} /> */}
     </Drawer.Navigator>
   );
