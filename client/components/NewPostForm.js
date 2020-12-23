bf71be7ae06cee0e04dd43652d6353e81b326974
Maxi De Filippis <maxidefilippis@gmail.com>
2020-12-22 23:52:56 -0300

    Update (/client/app + /client/NewPostForm) Modularizaicon del GetUserLogin

--- client/components/NewPostForm.js
+++ client/components/NewPostForm.js
@@ -1,58 +1,61 @@
-import React from "react";
-import { View, Text, TextInput,TouchableOpacity} from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { Formik } from "formik";
 import { styles } from "../styles/styles";
-import { CreatePost } from '../../database/controllers/controllerPost'
+import { createPost } from "../../database/controllers/controllerPost";
+import { getUserLogin } from "../functions/getUserLogin";
 
-export default function NewPostForm( {user} ){
+export default function NewPostForm() {
+  const [user, setUser] = useState(null);
 
-    const handlerPost = async (values) => {
-     var posteo = await CreatePost(values)
-        console.log(posteo)
-    }
+  const handlerPost = async (values) => {
+    var posteo = await createPost(values);
+    console.log(posteo);
+  };
 
-    return(
+  useEffect(() => {
+    getUserLogin().then((user) => setUser(user));
+  }, []);
 
-        <View style={styles.newpost} >
-            <Formik
-            initialValues={{title: "", description: "",tag: ""}}
-            onSubmit={handlerPost}
-            >
-                {(props) => (
-                    <View style={styles.form}>
-                        <Text style={styles.label}>Haz una publicación</Text>
-                        <TextInput
-                            
-                            placeholder="Haz tu pregunta"
-                            onChangeText={props.handleChange("title")} //update title
-                            value={props.values.title}                 //recibe valor de input
-                            style={styles.input}
+  return (
+    <View style={styles.newpost}>
+        
+      <Formik
+        initialValues={{ title: "", description: "", tag: "" }}
+        onSubmit={handlerPost}
+      >
+        {(props) => (
+          <View style={styles.form}>
+            { user && 
+            <Text style={{ color: "#FFF"}}>Hola {user.user.email} !</Text>
+            }
+            <Text style={{ color: "#FFF"}}>¿Tenés alguna duda?</Text>
+            <TextInput
+              placeholder="Haz tu pregunta"
+              onChangeText={props.handleChange("title")} //update title
+              value={props.values.title} //recibe valor de input
+              style={styles.input}
+            />
+            <TextInput
+              style={styles.inputdescription}
+              multiline //Permite texto largo
+              placeholder="Agregar una descripción"
+              onChangeText={props.handleChange("description")}
+              value={props.values.description}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Agregar etiqueta"
+              onChangeText={props.handleChange("tag")}
+              value={props.values.tag}
+            />
 
-                        />
-                        <TextInput
-                            style={styles.inputdescription}
-                            multiline  //Permite texto largo
-                            placeholder="Agregar una descripción"
-                            onChangeText={props.handleChange("description")} 
-                            value={props.values.description}                
-                        />
-                        <TextInput
-                            style={styles.input}    
-                            placeholder="Agregar etiqueta"
-                            onChangeText={props.handleChange("tag")} 
-                            value={props.values.tag}                
-                        />
-                        {/* <Button style={styles.boton} title="Publicar" onPress={props.handleSubmit} ></Button> */}
-
-                        <TouchableOpacity style={styles.boton} onPress={props.handleSubmit}>
-                          <Text style={{ fontWeight: "bold" }}>Publicar</Text>
-                        </TouchableOpacity>
-    
-
-                    </View>
-                )}  
-            </Formik>
-        </View>
-    )
+            <TouchableOpacity style={styles.boton} onPress={props.handleSubmit}>
+              <Text style={{ fontWeight: "bold" }}>Publicar</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
 }
-//Agregar etiquetas, foto 
