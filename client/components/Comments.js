1a94a447eaba171415103855b42743ef60751bfc
AyeVillarruel <ayelen.villarruel.1998@gmail.com>
2020-12-24 13:09:30 -0300

    update (/Comments) se guardan el comentario en un array

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect} from "react"
 import {StyleSheet, View, TextInput, Button, Alert } from "react-native"
 import Header from "../components/Header"
 import { styles } from "../styles/styles";
@@ -7,12 +7,16 @@ import { styles } from "../styles/styles";
 //Esto se tiene que renderizar en la pantalla postDetail
 export const Comments = ( {navigation} )=>{
 
-    /* const createAlert = () =>
-    Alert.alert(
-      "Listo!",
-      "Comentario posteado",
-      { cancelable: false }
-    ); */
+    const [comentario, setComentario] = useState(null)
+
+    let comentarios= []
+
+    
+
+    const enviarComentario = () => {
+      comentarios.push(comentario)
+      console.log('comentario',comentarios)
+    }
     
     return (
         <>
@@ -20,11 +24,14 @@ export const Comments = ( {navigation} )=>{
         <View style={styles.containerInput}>
             <TextInput placeholder="Escribe un comentario..." style={styles.comment}
             multiline = {true}
-            numberOfLines = {4}/>
+            numberOfLines = {4}
+            onChangeText={comentario => setComentario(comentario)}
+            value={comentario}
+            />
             <Button
         title="Comentar"
         color="#000000"
-       // onPress={createAlert}
+        onPress={enviarComentario}
       />
             
         </View>
