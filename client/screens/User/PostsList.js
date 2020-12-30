d0a0fbbc3186e203fa66bf176071230920291d1a
unknown <acevedonm@gmail.com>
2020-12-30 18:44:25 -0300

    Update (screens/User/PostList) Ahora muestra la foto de quien la creo, ademas refactorice la forma en la que mostramos la lista de posteos

--- client/screens/User/PostsList.js
+++ client/screens/User/PostsList.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, ScrollView } from "react-native";
+import { Text, View, ScrollView, FlatList } from "react-native";
 import { GetPosts } from "../../database/controllers/controllerPost";
 import { styles } from "../../styles/styles";
 // COMPONENTS //
 import Header from "../../components/Header";
 import SearchBar from "../../components/SearchBar";
+import { Avatar, ListItem, List } from 'react-native-elements'
 
-export default function Posts({ navigation }) {
+export default function PostsList({ navigation }) {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -20,6 +21,7 @@ export default function Posts({ navigation }) {
     GetPosts()
       .then((posts) => {
         setPosts(posts.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+
       })
       .then(() => setLoading(false))
       .catch((err) => {
@@ -27,38 +29,46 @@ export default function Posts({ navigation }) {
       });
   }, []);
 
+
+  const keyExtractor = (item, index) => index.toString()
+
+  const renderItem = ({ item }) => (
+    <ListItem bottomDivider >
+      <Avatar title={item.title[0]} source={item.photo && { uri: item.photo }}/>
+      <ListItem.Content>
+        <ListItem.Title>{item.title}</ListItem.Title>
+        <ListItem.Subtitle>{item.tag}</ListItem.Subtitle>
+      </ListItem.Content>
+      <ListItem.Chevron />
+    </ListItem>
+  )
+
+
   return (
     <>
+      {console.log("Set Post: ", posts)}
       <Header navigation={navigation} />
       <ScrollView>
         <View style={styles.bodyPostList}>
           <SearchBar onSearch={handleSearch} />
           <View style={styles.cardPostList}>
-            { loading ? <Text>Cargando posts..</Text> : 
-            <>
-            <Text style={styles.h3}>ULTIMAS ENTRADAS</Text>
-            {posts && posts.length >= 1 ? (
+            {loading ? <Text>Cargando posts..</Text> :
               <>
-                {posts.map((post) => (
-                  <View key={post.id} style={styles.post}>
-                    <Text
-                      style={styles.postList}
-                      onPress={() =>
-                        navigation.navigate("PostDetails", { data: post})
-                      }
-                    >
-                      {post.title.length > 20
-                        ? `${post.title.substring(0, 20)}..`
-                        : post.title}
-                    </Text>
-                    <Text style={styles.postTag}>{post.tag}</Text>
-                  </View>
-                ))}
+                <Text style={styles.h3}>ULTIMAS ENTRADAS</Text>
+                {posts && posts.length >= 1 ? (
+                  <>
+                    <>
+                      <FlatList
+                        keyExtractor={keyExtractor}
+                        data={posts}
+                        renderItem={renderItem}
+                      />
+                    </>
+                  </>
+                ) : (
+                    <Text>No se encontraron resultados..</Text>
+                  )}
               </>
-            ) : (
-              <Text>No se encontraron resultados..</Text>
-            )}
-            </>
             }
           </View>
         </View>
